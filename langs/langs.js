7d0328b47867bac70e5a850a2e2e771964b61f41
Mari <mvihuri@gmail.com>
2022-05-04 16:04:46 -0400

    add more polls

--- langs/langs.js
+++ langs/langs.js
@@ -1,19 +1,23 @@
-const show_lang = document.getElementById("show_lang");
-const show_langs = document.getElementById("show_langs");
 const a_lang = document.getElementById("a_lang");
 const a_langs = document.getElementById("a_langs");
 const dom_lang = document.getElementById("dom_lang");
 const dom_langs = document.getElementById("dom_langs");
 const load_lang = document.getElementById("load_lang");
 const load_langs = document.getElementById("load_langs");
+const a_lang_poll = document.getElementById("a_lang_poll");
+const a_langs_poll = document.getElementById("a_langs_poll");
+const dom_lang_poll = document.getElementById("dom_lang_poll");
+const dom_langs_poll = document.getElementById("dom_langs_poll");
+const load_lang_poll = document.getElementById("load_lang_poll");
+const load_langs_poll = document.getElementById("load_langs_poll");
 
 document.addEventListener('readystatechange', () => console.log("document.readyState " + document.readyState));
 
 console.log("whenever: " + navigator.language);
 a_lang.innerHTML = navigator.language;
 a_langs.innerHTML = navigator.languages;
-show_lang.innerHTML = navigator.language;
-show_langs.innerHTML = navigator.languages;
+a_lang_poll.innerHTML = navigator.language;
+a_langs_poll.innerHTML = navigator.languages;
 
 
 const obj = window.navigator;
@@ -24,8 +28,12 @@ function keepChecking(oldValue) {
      if(obj.language !== oldValue) {
       console.log("poll: " + obj.language);
       oldValue = obj.language;
-      show_lang.innerHTML = navigator.language;
-      show_langs.innerHTML = navigator.languages;
+      a_lang_poll.innerHTML = navigator.language;
+      a_langs_poll.innerHTML = navigator.languages;
+      dom_lang_poll.innerHTML = navigator.language;
+      dom_langs_poll.innerHTML = navigator.languages;
+      load_lang_poll.innerHTML = navigator.language;
+      load_langs_poll.innerHTML = navigator.languages;
       }
     }, 3000);
   }
@@ -37,14 +45,14 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("dom content loaded: " + navigator.language);
     dom_lang.innerHTML = navigator.language;
     dom_langs.innerHTML = navigator.languages;
-    show_lang.innerHTML = navigator.language;
-    show_langs.innerHTML = navigator.languages;
+    dom_lang_poll.innerHTML = navigator.language;
+    dom_langs_poll.innerHTML = navigator.languages;
 });
 
   window.addEventListener('load', (event) => {
     console.log("window loaded: " + navigator.language);
     load_lang.innerHTML = navigator.language;
     load_langs.innerHTML = navigator.languages;
-    show_lang.innerHTML = navigator.language;
-    show_langs.innerHTML = navigator.languages;
+    load_lang_poll.innerHTML = navigator.language;
+    load_langs_poll.innerHTML = navigator.languages;
 });
